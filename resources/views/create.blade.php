1ebf030b40c19ad538aa878bc29af6e72fadca7f
MAGG <alexandragalarzag@gmail.com>
2023-02-08 18:02:01 +0100

    head in a layout folder

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Page</title>
-</head>
+@extends('layouts.head')
 
 <body>
     <form action="{{ route('store') }}" method="POST" enctype="multipart/form-data">
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- cambiar carpeta css a public --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Our Nice Phrases</title>
+</head>
