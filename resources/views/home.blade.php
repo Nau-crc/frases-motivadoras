778cf37aa98064498e2463a9978a93b36cec3c55
MAGG <alexandragalarzag@gmail.com>
2023-02-08 17:50:01 +0100

    show button in home page and styles for images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- cambiar carpeta css a public --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Home Page</title>
 </head>
 
@@ -12,11 +14,11 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                                                                <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                                                                            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
 @else
     <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                                                                @if (Route::has('register'))
+                                                                                            @if (Route::has('register'))
     <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
     @endif
                     @endauth
@@ -30,7 +32,10 @@
             @foreach ($phrases as $phrase)
                 <h1>{{ $phrase->phrase }}</h1>
                 <p>{{ $phrase->author }}</p>
-                <img src="{{ $phrase->image }}">
+                <div class="ct-phrase-img">
+                    <img src="{{ $phrase->image }}" class="phrase-img" alt="{{ $phrase->phrase }}">
+                </div>
+                <a href="{{ route('show', ['id' => $phrase->id]) }}"><button>See Phrase</button></a>
                 <a href="{{ route('edit', ['id' => $phrase->id]) }}"><button>Edit Phrase</button></a>
                 <form action="{{ route('destroy', ['id' => $phrase->id]) }}" method="POST">
                     @method('delete')
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Show Page</title>
 </head>
 
@@ -12,8 +13,10 @@
     <a href="{{ route('home') }}"><button>Regresar</button></a>
     <h2>{{ $phrase->phrase }}</h2>
     <p>{{ $phrase->author }}</p>
+    <div class="ct-show-phrase-img">
+        <img src="{{ $phrase->image }}" alt="{{ $phrase->phrase }} . 'photo'" class="phrase-img">
+    </div>
 
-    <img src="{{ $phrase->image }}" alt="{{ $phrase->phrase }} . 'photo'">
 </body>
 
 </html>
