c25e631fe68465d256bd98e8a5011574976f2467
Laura Rodriguez <laura.rodcarrionm@gmail.com>
2023-02-27 12:55:55 +0100

    testing half way

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,45 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
+
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+
+    public function test_home_page_exists()
     {
-        $response = $this->get('/');
+        $response = $this->get('/home');
 
         $response->assertStatus(200);
     }
+
+    public function test_show_phrase_exists()
+    {
+        $response = $this->get('/show/1');
+
+        $response->assertStatus(200);
+    }
+    public function test_edit_page_exists()
+    {
+        $response = $this->get('/edit/1');
+
+        $response->assertStatus(200);
+    }
+    public function test_user()
+    {
+        $user = User::factory()->create();
+ 
+        $response = assertExists();
+    }
+
+
 }
